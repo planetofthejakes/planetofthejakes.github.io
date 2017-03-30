aa0410657ddf3fe23469acc1ce62e6aba9051ffc
Jake <jakeschulte@Jakes-MacBook-Pro-2.local>
2017-03-29 21:11:45 -0700

    background video added

---
+++ final_project_boilerplate/js/main.js
@@ -0,0 +1,10 @@
+// // // get started button the landing page 
+// $('.get-started').find('button').on('click', function (e) {
+//   e.preventDefault();
+
+//   
+//   $(this).addClass('active');
+// });
+
+
+
