8b8a5b6b8205f24b228811537ff94c43276e934c
Jake <jakeschulte@Jakes-MacBook-Pro-2.local>
2017-04-03 16:09:38 -0700

    natural language form

--- final_project_boilerplate/js/main.js
+++ final_project_boilerplate/js/main.js
@@ -1,10 +1,30 @@
-// // // get started button the landing page 
-// $('.get-started').find('button').on('click', function (e) {
-//   e.preventDefault();
+// get started button scroll to next page
 
-//   
-//   $(this).addClass('active');
-// });
+$('.get-started').on('click', function () {
+    // Type code related to event here! :)
+    $('html,body').animate({
+    	scrollTop: $('.page-2').offset().top
+    	},'slow');
+});
+
+// natural language form
+
+
+// PSEUDO for the transition of the list overlay
+// hide ul
+// on click of nl-field-toggle
+	// if hidden, show ul overlay
+	// when user selects option
+		// replace the toggle value with new value
+		// fade.out ul overlap
+
+$('select').hide();
+
+$('.nl-field-toggle').on('click', function (e) {
+    // Type code related to event here! :)
+    e.preventDefault();
+    $('.nl-field').toggleClass('nl-field-open');
+});
 
 
 
