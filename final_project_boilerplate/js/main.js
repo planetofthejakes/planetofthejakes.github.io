f951cae05337d7eb13f70b30a127a16174e7a312
Jake <jakeschulte@Jakes-MacBook-Pro-2.local>
2017-04-05 14:45:46 -0700

    final draft? hm.

--- final_project_boilerplate/js/main.js
+++ final_project_boilerplate/js/main.js
@@ -1,26 +1,19 @@
 
-// natural language form variables
-
+// natural language form variables (JS) for ul selection process
 var genre = document.getElementById('genre'),
 	genreSelect = document.getElementById('genre-select'),
 	length = document.getElementById('length'),
 	lengthSelect = document.getElementById('length-select');
 
 
-// get started button scroll to next page
+// get started button scroll to page-2
 
 $('.get-started').on('click', function () {
     // Type code related to event here! :)
+    // on button click, animate the page down and scroll to top of page-2, slow speed
     $('html,body').animate({
     	scrollTop: $('.page-2').offset().top
-    	},'slow');
-});
-
-$('.nl-submit-wrap').on('click', function () {
-    // Type code related to event here! :)
-    $('html,body').animate({
-    	scrollTop: $('.page-3').offset().top
-    	},'slow');
+    	},700);
 });
 
 // $('.genre-list li').on('click', function(e) {
@@ -28,19 +21,12 @@ $('.nl-submit-wrap').on('click', function () {
 //	alert('Click');
 //}
 
-// natural language form
+// NATURAL LANGUAGE FORM BELOW
 
-
-// PSEUDO for the transition of the list overlay
-// hide ul
-// on click of nl-field-toggle
-	// if hidden, show ul overlay
-	// when user selects option
-		// replace the toggle value with new value
-		// fade.out ul overlap
-
+// hide select class, create path to find value below
 $('select').hide();
 
+// toggle ul on click
 $('.nl-field-toggle').on('click', function (e) {
     // Type code related to event here! :)
     e.preventDefault();
@@ -57,12 +43,14 @@ $('.nl-field-toggle-2').on('click', function (e) {
 
 $('.genre-list li').on('click', function (e, el) {
 	e.preventDefault();
+	// JS to target and change HTML text in genre list
 	var genreText = e.target.innerHTML,
+		// index returns the index position of specified elements relative to other specified elements
 		index = $('.genre-list li').index(this);
-
+	// 	get index value of genre selection and change selected HTML text field
 	genre.innerHTML = genreText;
 	genreSelect.getElementsByTagName('option')[index + 1].selected = true;
-
+	// toggle off ul on selection of li item
 	$('.nl-field').toggleClass('nl-field-open');
 	console.log($('#genre-select').val());
 })
@@ -79,29 +67,33 @@ $('.length-list li').on('click', function (e, el) {
 	console.log($('#length-select').val());
 })
 
+// FIND MOVIE button from page-2 to page-3
+$('.nl-submit-wrap').on('click', function () {
+    // Type code related to event here! :)
+    $('html,body').animate({
+    	scrollTop: $('.page-3').offset().top
+    	},700);
+    // hide gif that comes up if user clicks .nic-cage button
+    $('.nic-gif, p').hide();
+});
 
-// movie selection 
 
-// pseudo code
-// on.click of button .nl-submit
-	// pull values of selected options
-	// scroll down page
-	// display movie that = those set of values
-		// if value total = 2
-			// show con air trailer
-		// else show national treasure
+// FILM SELECTION 
+
 
 $('.page-3').hide();
 $('.con-air').hide();
 $('.national-treasure').hide();
 
-$('.nl-submit-wrap').on('click', function(e) {
+$('.nl-submit').on('click', function(e) {
 	e.preventDefault();
-
+// attempt number 2
 	// var value = $('option').val();
+	// find value of genre li item and length li item
 	var selectedGenre = $('#genre-select').val(),
 		selectedLength = $('#length-select').val();
 
+// attempt number 1 
 	// if (value === 1) {
 	// 	$('.page-3, .con-air').show();
 	// } else {
@@ -114,6 +106,8 @@ $('.nl-submit-wrap').on('click', function(e) {
 	// 	$('.page-3, .national-treasure').hide();
 	// }
 
+	// if genre value is 1 and length value is 1 then show con-air, else national treasure
+	// need "" around number so value and number can equal each other
 	if (selectedGenre === "1" && selectedLength === "1") {
 		$('.page-3, .con-air').show();
 	} else {
@@ -125,16 +119,32 @@ $('.nl-submit-wrap').on('click', function(e) {
 
 // .page-3 button actions
 
+// hide gif and giphy p text
+$('.nic-gif').hide();
+$('p').hide();
+
 $('.new-movie').on('click', function () {
     // Type code related to event here! :)
+    // on button click scoll back up to top of page 2 and hide page 3
     $('html,body').animate({
     	scrollTop: $('.page-2').offset().top
-    	},'slow');
+    	},700);
+    $('.page-3').hide();
+});
+
+$('.nic-cage').on('click', function () {
+    // Type code related to event here! :)
+    // on button click scoll back up to top of page 2 and hide page 3
+    // show gif and p text 
+    $('html,body').animate({
+    	scrollTop: $('.page-2').offset().top
+    	},700);
+    $('.page-3').hide();
+    $('.nic-gif, p').show();
 });
 
 
 // hidden nic cage gif on .page-2
-
 
 
 
